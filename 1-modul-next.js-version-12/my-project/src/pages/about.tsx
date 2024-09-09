119bf35ac03a5ea8297cfc6d85a55f95f471697a
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-09-09 11:00:21 +0500

    sss

---
+++ 1-modul-next.js-version-12/my-project/src/pages/about.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const AboutPage = () => {
+    return (
+        <>
+            <Head>
+                <title>About page</title>
+                <meta
+                    name="description"
+                    content="Seo uchun juda kerakli joyi"
+                />
+            </Head>
+            <div>AboutPage</div>
+        </>
+    );
+};
+
+export default AboutPage;
