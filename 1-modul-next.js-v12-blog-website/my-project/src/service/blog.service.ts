9ddcd93d5f2f9389e749ac72a182987a0ff19131
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-09-15 14:13:45 +0500

    sss

--- 1-modul-next.js-v12-blog-website/my-project/src/service/blog.service.ts
+++ 1-modul-next.js-v12-blog-website/my-project/src/service/blog.service.ts
@@ -1,12 +1,13 @@
 //yarn add graphql graphql-request// hygraphda qilingan tayyor serverni ishlatish uchun kerak bo'ladigan librariylar lekin bular yangi versiyalari bo'lgani uchun ishlamadi shu nom bilan bir hil lekin dars qilingan paytdagi versiyasi bu eski versiyasini download qilish buyrug'i>>>yarn add graphql-request@5.1.0 <<<shu bilan ishladi
 
-import { gql, request } from "graphql-request";
+import { request, gql } from "graphql-request";
+import { BlogsType } from "src/interface/blogs.interface";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT as string;
 
 export const BlogsService = {
     //constructor
-    9. GraphQL darsida hato bor
+    // 9. GraphQL darsida hato bor
     async getAllBlogs() {
         const query = gql`
             query GetBlogs {
@@ -32,7 +33,7 @@ export const BlogsService = {
             }
         `;
 
-        const result = await request(graphqlAPI, query);
-        return result;
+        const result = await request<{ blogs: BlogsType[] }>(graphqlAPI, query); //hygraph serverdan blogs nomli object qatradi massiv ichida yani onasi massiv bo'lgan object qaytradi  // serverdan await bo'lib keladigan datalarni genericga olib  blogs deb nom berib bu serverdan keladigan datalarni type interface papka ichidagi blogs.interface.ts fileda yozilgana BlogsType bo'lsin deyildi blogstypeda hygraph serverdan keladigan datalarni typlari yozilgan... tsda agar typlar yozilmasa hato chiqadi shu sabab hamma datalarni funksiyalarni type bo'lishi shart
+        return result.blogs//yani bu funksiya qaytarsin reusult o'zgaruvchini va ichidagi blogsni blogs esa ts uchun qilingan tipizatsa hissoblanadi 
     },
 };
