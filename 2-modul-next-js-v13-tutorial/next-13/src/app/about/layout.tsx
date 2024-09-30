77c7ab4684d10ac00e550e56e5389de9bd97d754
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-09-30 23:21:55 +0500

    dsds

---
+++ 2-modul-next-js-v13-tutorial/next-13/src/app/about/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About page",
+    description: "Biz Haqimizda ",
+    keywords: "",
+    robots: "",
+    // robots:""????????????????????? SHU SCHEMA EMASMI SEO UCHUN
+};
+
+export default function AboutLayout({
+    // bu children about papkani ichidagi page.tsx fili hissoblanadi yani AboutLayout kalit so'z yani next endi childrenga about papkani ichidagi pageni children qilib qo'yadi shunda about pageda bu sahifa ona sahifa yani root hissoblanadi va childreni abot papka ichidagi page bo'ladi asosiy rootda esa RootLayout deb yoziladi agar endi boshqa sahifa ochish kerak bo'lsa o'sha sahifaga root file qilib layoutdan oldin o'sha sahifa joylashgan papkani nomi yozilishi kerak masalan AboutLayout emas ContactLayout shu klalit so'zlardan next  qanday routing qilishni biladi
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <section>
+            <p>Start About</p>
+            {children}
+            <p>End About</p>
+        </section>
+    );
+}
