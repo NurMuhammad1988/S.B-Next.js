d11d564475fc527e454a0a0e7af61d3335bc28ce
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-11-03 06:44:06 +0500

    sss

--- 4-modul Twitter Clone 14v/twitter-x/components/shared/form.tsx
+++ 4-modul Twitter Clone 14v/twitter-x/components/shared/form.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { IUser } from "@/types";
 import { useState } from "react";
@@ -14,25 +14,33 @@ interface Props {
     // setPosts: Dispatch<SetStateAction<IPost[]>>
 }
 
-6. Post form & fetching data 04:54 da qoldi
+// 6. Post form & fetching data === 04:54 da qoldi
 
 const Form = ({ placeholder, user }: Props) => {
     const [body, setBody] = useState("");
     const [isLoading, setIsLoading] = useState(false);
-    const onSubmit = async () => {}
+    const onSubmit = async () => {};
 
     return (
         <div className="border-b-[1px] border-neutral-800 px-5 py-2">
             <div className="flex flex-row gap-4">
                 <Avatar>
                     <AvatarImage src={user.profileImage} />
+
                     <AvatarFallback>{user.name[0]}</AvatarFallback>
                 </Avatar>
 
                 <div className="w-full">
+                    <textarea
+                        className="disabled:opacity-80 peer resize-none mt-3 w-full bg-black ring-0 outline-none text-[20px] placeholder-neutral-500 text-white h-[50px]"
+                        placeholder={placeholder}
+                        disabled={isLoading}
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                        onKeyDown={(e) => e.key === "Enter" && onSubmit()}
+                    ></textarea>
 
-
-
+                    <hr className="opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition" />
                 </div>
             </div>
         </div>
