0a8a32be4ab6dbf6a4bee072c0116c61ce35da3f
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-11-05 02:28:42 +0500

    ssss

--- 4-modul Twitter Clone 14v/twitter-x/app/api/auth/login/route.ts
+++ 4-modul Twitter Clone 14v/twitter-x/app/api/auth/login/route.ts
@@ -1,37 +1,37 @@
-// import User from "@/database/user.model";
-// import { connectToDatabase } from "@/lib/mongoose";
-// import { compare } from "bcrypt";
-// import { NextRequest, NextResponse } from "next/server";
+import User from "@/database/user.model";
+import { connectToDatabase } from "@/lib/mongoose";
+import { compare } from "bcrypt";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function POST(req: Request) {
-//     try {
-//         await connectToDatabase();
-//         const { email, password } = await req.json();
+export async function POST(req: Request) {
+    try {
+        await connectToDatabase();
+        const { email, password } = await req.json();
 
-//         const isExistingUser = await User.findOne({ email });
+        const isExistingUser = await User.findOne({ email });
 
-//         if (!isExistingUser) {
-//             return NextResponse.json(
-//                 { error: "Email does not exist" },
-//                 { status: 400 }
-//             );
-//         }
+        if (!isExistingUser) {
+            return NextResponse.json(
+                { error: "Email does not exist" },
+                { status: 400 }
+            );
+        }
 
-//         const isPasswordValid = await compare(
-//             password,
-//             isExistingUser.password
-//         );
+        const isPasswordValid = await compare(
+            password,
+            isExistingUser.password
+        );
 
-//         if (!isPasswordValid) {
-//             return NextResponse.json(
-//                 { error: "Password is incorrect" },
-//                 { status: 400 }
-//             );
-//         }
+        if (!isPasswordValid) {
+            return NextResponse.json(
+                { error: "Password is incorrect" },
+                { status: 400 }
+            );
+        }
 
-//         return NextResponse.json({ success: true, user: isExistingUser });
-//     } catch (error) {
-//         const result = error as Error;
-//         return NextResponse.json({ error: result.message }, { status: 400 });
-//     }
-// }
+        return NextResponse.json({ success: true, user: isExistingUser });
+    } catch (error) {
+        const result = error as Error;
+        return NextResponse.json({ error: result.message }, { status: 400 });
+    }
+}
