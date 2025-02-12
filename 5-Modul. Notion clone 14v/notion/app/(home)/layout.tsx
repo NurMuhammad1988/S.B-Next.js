5de3afc14b6bc0d82314cb117d7d84df5ef8450d
NurMuhammad1988 <nurikyorov88@gmail.com>
2025-02-13 02:45:34 +0500

    ddd

---
+++ 5-Modul. Notion clone 14v/notion/app/(home)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//>>>()<<< shu haqida comment yoz bu layout shu ()<<ni ichida commnt??
+
+//5 chi modul 1. Bosh sahifa darsi 5:57 da qoldi
+
+const Homelayaot = () => {
+  return (
+    <div>Homelayaot</div>
+  )
+}
+
+export default Homelayaot
