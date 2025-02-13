c3061edd2a820cf8473695f76ab9cae75137565c
NurMuhammad1988 <nurikyorov88@gmail.com>
2025-02-14 02:30:14 +0500

    dddd

--- 5-Modul. Notion clone 14v/notion/app/(home)/page.tsx
+++ 5-Modul. Notion clone 14v/notion/app/(home)/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
-// asosiy sahifa
+// asosiy sahifa yuqorida homelayout chunki sitebar pastda esa kontent joylashadi
 
-//>>>()<<< shu haqida comment yoz
+//>>>(home)<<< bu dumaloq skopka ichidagi page.tsx shuni anglatadiku bu asosiy sahifa hissoblanadi yani next js ishlaganda appni ichidan asosiy sahifani yani failni izlaydi papkani emas failni yani page.tsx failini izlaydi agar app papkani ichidan page.tsx failni topolmasa (home) papkani ichidan izlaydi va topsa shu page.tsx failni asosiy sahifa qiladi va agar (home)ni ichiga yana (home2) qilinsa shunday skopkalarni ichidan kirib kirib page.tsxni topib shuni asosiy sahifa qiladi
 
 export default function Home() {
-    return <div>Hello</div>;
+    return <div>Hello page</div>;
 }
