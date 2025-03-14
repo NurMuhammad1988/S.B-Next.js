c0e2aedc9ed2680a8f148fffd0c6a225465eb01d
NurMuhammad1988 <nurikyorov88@gmail.com>
2025-03-14 23:35:17 +0500

    sdddd

--- 5-Modul. Notion clone 14v/notion/app/(secret)/components/sidebar.tsx
+++ 5-Modul. Notion clone 14v/notion/app/(secret)/components/sidebar.tsx
@@ -6,14 +6,16 @@ import React, { ElementRef, useRef, useState } from "react";
 export const Sidebar = () => {
     const sidebarRef = useRef<ElementRef<"div">>(null);
     const navbarRef = useRef<ElementRef<"div">>(null); //Elementlar React ilovalarining eng kichik qurilish bloklaridir . Element foydalanuvchi interfeysida nima bo'lishi kerakligini belgilaydi. Element - bu DOM tugunlari nuqtai nazaridan biz nimani ko'rishni xohlayotganimizni tavsiflovchi oddiy ob'ekt. Reaktsiya elementini yaratish DOM elementlariga nisbatan oson. Element JSX yordamida yoki JSXsiz React yordamida yaratilishi mumkin.//yani bu holatda ElementRef objecti bilan div yaratib boshlang'ich qiymati null qilindi
+    const isResizing = useRef(false);
 
     const [isCollapsed, setIsCollapsed] = useState(false);
     const [isResetting, setIsResetting] = useState(false);
 
-    //ref uchun eng keng tarqalgan foydalanish holati DOM elementiga kirishdir . Misol uchun, agar siz kiritilgan ma'lumotlarni dasturiy tarzda qaratmoqchi bo'lsangiz, bu qulay. Refni JSX da <div ref={myRef}> kabi ref atributiga o'tkazganingizda, React mos keladigan DOM elementini myRef.current ga joylashtiradi.////////////bu holatda siebarRefga joylashtiradi yani pastdagi ona divga joylashtiradi shunda ona divni collapse functionga chaqirvolib if else holatiga qarab stylelarini o'zgartirish mumkun masalan bu holatda yuqorida stete bilan sidebarRefdagi nul divi collapse functionga chaqirilib qaytadan yaratilib keraklistylelari o'zgartirilayapti yani sidebarRefda chaqirilgan ElementRef ichidagi bo'sh diviga pastdagi ona divni chaqirib collapse functioni bilan faqat kerakli joyiga style o'zgartirishlar kiritayapti bu uchun ElementRef currentga pastdagi divni sovoladi va agar ichida pastdagi divi bor sidebarRef o'zgaruvchini currentida pastdagi div kelgan bo'lsa va huddi shunday  navbarRef o'zgaruvchi ichidagi currentda navbarRef divi kelgan bo'lsa boshida false bo'lgan setIsCollapsed va setIsResetting true qilib sidebarda kelgan yangi divga current bilanqaytadan tyle berildi masalan widthi 0 qilindi va navbarref diviniki esa 100% qilindi va lefti 0 qilindi bu reflardagi currentlarga aynan shu divlar tushishi uchun pastdagi shu divlarga ref={sidebarRef},  ref={navbarRef} deb div atribut bilan berilib qo'yilishi kerak shunda ElementRef qaysi divga borib nusxa olib kelishni biladi (ref yani adress degani) shunda pastdan elementrerf sabab kelgan divlarni stylelari shu holatga o'zgaradi bunga sabab pastdagi divlargdagi berilgan onclickda (iconlarga) collapse va reset functionlari berib qo'yilgan yani ona diviga berilishi kerak!!!
+    //ref uchun eng keng tarqalgan foydalanish holati DOM elementiga kirishdir . Misol uchun, agar siz kiritilgan ma'lumotlarni dasturiy tarzda qaratmoqchi bo'lsangiz, bu qulay. Refni JSX da <div ref={myRef}> kabi ref atributiga o'tkazganingizda, React mos keladigan DOM elementini myRef.current ga joylashtiradi.////////////bu holatda siebarRefga joylashtiradi yani pastdagi ona divga joylashtiradi shunda ona divni collapse functionga chaqirvolib if else holatiga qarab stylelarini o'zgartirish mumkun masalan bu holatda yuqorida stete bilan sidebarRefdagi nul divi collapse functionga chaqirilib qaytadan yaratilib keraklistylelari o'zgartirilayapti yani sidebarRefda chaqirilgan ElementRef ichidagi bo'sh diviga pastdagi ona divni chaqirib collapse functioni bilan faqat kerakli joyiga style o'zgartirishlar kiritayapti bu uchun ElementRef currentga pastdagi divni sovoladi va agar ichida pastdagi divi bor sidebarRef o'zgaruvchini currentida pastdagi div kelgan bo'lsa va huddi shunday  navbarRef o'zgaruvchi ichidagi currentda navbarRef divi kelgan bo'lsa boshida false bo'lgan setIsCollapsed va setIsResetting true qilib sidebarda kelgan yangi divga current bilan qaytadan style berildi masalan widthi 0 qilindi va navbarref diviniki esa 100% qilindi va lefti 0 qilindi bu reflardagi currentlarga aynan shu divlar tushishi uchun pastdagi shu divlarga ref={sidebarRef},  ref={navbarRef} deb div atribut bilan berilib qo'yilishi kerak shunda ElementRef qaysi divga borib nusxa olib kelishni biladi (ref yani adress degani) shunda pastdan elementref sabab kelgan divlarni stylelari shu holatga o'zgaradi bunga sabab pastdagi divlargdagi berilgan onclickda (iconlarga) collapse va reset functionlari berib qo'yilgan yani ona diviga berilishi kerak!!!
 
     //elementref sabab pastdan nusxa olingan divdan kelgan setIsResettingda animatsya yani sekin kirib sekin chiqish stylelari bor yani bu holatda setIsResetting boshida false state agar collapse functiondagi if true bo'lsa setIsResettingham true bo'ladi yani style "0", "100%", "0" ga o'zgarganda bu animatsya styleham qo'shiladi shunda chap tomonga sekin kirib sekin chiqadi😏😏😏  endi truega aylangan isResetting esa umumiy ona divlarga if bilan berib qo'yilgan chunki bu isResetting ona div ichidagi evenlarga qarab ishlashi kerak chunkiiconlar ona div ichida
     const collapse = () => {
+        //ChevronsLeft iconiga click bo'lganda ishlaydi
         if (sidebarRef.current && navbarRef.current) {
             setIsCollapsed(true);
             setIsResetting(true);
@@ -23,15 +25,47 @@ export const Sidebar = () => {
         }
     };
 
-   
     const reset = () => {
+        //MenuIcon iconiga bosilganda ishlaydi
         if (sidebarRef.current && navbarRef.current) {
             setIsCollapsed(false); //collapse function onclick bo'lib setIsCollapsed true bo'lib sidebarni  widthi 0 bo'lib navbarRefni widthi 100% bo'lgandan keyin setIsCollapsed false qilinadi chunki endi pastdagi "240px", "calc(100% - 240)", "240" stylelari ishlashi kerak yani MenuIcon iconi chiqishi kerak
             setIsResetting(true);
             sidebarRef.current.style.width = "240px"; //MenuIcon iconiga bosilganda 240px joy ochadi
-            navbarRef.current.style.width = "calc(100% - 240)"; //MenuIcon bosilganda calc bilan navbarref uchun 100% widthdan 240px joy ochadi
+            navbarRef.current.style.width = "calc(100% - 240)"; //MenuIcon bosilganda calc bilan navbarref uchun 100% widthdan 240px joy ochadi yani ayirib tashaydi
             navbarRef.current.style.left = "240"; //MenuIcon bosilganda joyni chap tomondan ochadi
         }
+    };
+
+    const handleMouseDown = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        event.preventDefault(); //preventDefault() hodisa metodi. Ushbu usul brauzerning hodisa ko'rib chiqilayotganda paydo bo'ladigan standart xatti-harakatlarini bekor qiladi . Misol uchun, biz havolani bosganimizda, biz ushbu havolaning manziliga o'tamiz. preventDefault() ni chaqirish bu xatti-harakatni bekor qiladi.
+        event.stopPropagation(); //bu hodisani faqat mouse click bo'lgan joyda ishlatish yani faqat click qilingan joyda ishlaydi butun windiwga tasir qilmaydi
+
+        isResizing.current = true; //ref berilgan isresizing boshida false edi endi shunui currentini true qilindi yani refgatasir qilaolish uchun hodisa ilish uchun
+        document.addEventListener("mousemove", handleMouseMove); //umumiy dacumentdan yani js dacumentidan addEventListener metod functionini  "mousemove" qiymati chaqirildi bu "mousemove" tozza jsdan keladigan hodisa ilish uchun ishlatiladigan qiymat yani mishka qimillaganda bu addeventlistinerni birinchi parametriga yoziladi handleMouseMove function esa shu "mousemove" holati ishlaganda birgalikda ishlaydigan function
+        document.addEventListener("mouseup", handleMouseUp);
+    };
+
+    const handleMouseMove = (event: MouseEvent) => {
+        if (!isResizing.current) return; // agar isResizing false bo'lsa hech qanday mouse hodisasi bo'lmasin
+        let newWidth = event.clientX; // global let object newWidth ichida hodisa ovoldik yani x o'qi bo'yicha windowni newWidthga ovoldik
+        if (newWidth < 240) newWidth = 240; //agar newWidth x o'qi bo'yichda 240pxdan kam bo'lsa newWidthni yana 240 qilib qo'yish yani sidebar 240pxdan kam bo'lmasligi kerak agar user hohlasa faqat ko'p bo'lishi kerak
+        if (newWidth > 400) newWidth = 400; //yani sidebar 400pxgacha chapga o'sadi undan ko'p emas shunda user sidebarni kamida 240 ko'pi bilan 400pxgacha cho'zaoladi
+
+        if (sidebarRef.current && navbarRef.current) {
+            // sidebarRef.current && navbarRef.current true bo'lsa yangi hodisalar ilinayapti
+            sidebarRef.current.style.width = `${newWidth}px`; //newWidth ni pxlda berish aytib qo'yildi yani bu holatda handleMouseMove functioni ishlaganda
+            navbarRef.current.style.left = `${newWidth}px`;
+            navbarRef.current.style.width = `calc(100% - ${newWidth}px)`;
+        }
+    };
+
+    const handleMouseUp = () => {
+        //mishka qo'yvorilgandan keyin ishga tushadigan hodisa functioni
+        isResizing.current = false;
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", handleMouseUp);
     };
 
     return (
@@ -41,7 +75,7 @@ export const Sidebar = () => {
                     "group/sidebar  h-screen bg-secondary overflow-y-auto relative flex w-60 flex-col z-50",
                     isResetting && "transition-all ease-in  duration-300"
                 )}
-                ref={sidebarRef}//elementref ishlashi uchun ref jsxda shunday atribut bilan berib qo'yilishi kerak
+                ref={sidebarRef} //elementref ishlashi uchun ref jsxda shunday atribut bilan berib qo'yilishi kerak
             >
                 {/* bu ona divdagi group classi bola divgaham yozildi shunda bitta class ikkita divga bir hil ishlaydi masalan pastdagi yetim divda group-hover:opacity-100" classi bor bu degani pastdagi divda cursor tursa hover bo'lib shu classlar ishlaydi yani pastdagi divda opacity 0 bo'lib turgabi group-hover:opacity-100" ga o'zgaradi yani cursor faqat pastdagi yetim divga o'tganda shu group-hover:opacity-100" hover klasslari ishlaydi .....................group/sidebar va  group-hover/sidebar:opacity-100 deb yozilishini sababi esa bu sahifada yana shunaqa grouplar qilinganda hammasi ishlashi uchun masalan groupni o'zi bilan qilsa faqat shu group ishlaydi agar bu groupga nom berilsa (bu holatda sidebar deb nom berildi chunki faqat sidebar uchun kerakli group) har hil nomdagi hamma grouplar ishlaydi */}
 
@@ -54,7 +88,10 @@ export const Sidebar = () => {
                     <ChevronsLeft className="h-6 w-6" />{" "}
                 </div>
 
-                <div className="absolute right-0 top-0 w-1 h-full cursor-ew-resize bg-primary/10 opacity-0 group-hover/sidebar:opacity-100 transition" />
+                <div
+                    className="absolute right-0 top-0 w-1 h-full cursor-ew-resize bg-primary/10 opacity-0 group-hover/sidebar:opacity-100 transition"
+                    onMouseDown={handleMouseDown}
+                />
                 {/* bu yetim div yani sidebar qismini qolgan asosiy qisimdan ajratish uchun yani tepadan pastga to'g'ri chiziq tortish uchun cursor-ew-resize classi esa shu chiziqga kelganda cursorni chap o'ng tarafgaham strelkali cursor chiqaradi>>> ↔ ↔ ↔ ↔ ↔ <<< yani ekrandagi sidebar va qolgan qismlarni o'lchamini o'zgartirish uchun   */}
             </div>
 
@@ -78,5 +115,3 @@ export const Sidebar = () => {
         </>
     );
 };
-
-3. Sidebar resizing darsi 16:53 da qoldi
--- 5-Modul. Notion clone 14v/notion/app/(secret)/documents/page.tsx
+++ 5-Modul. Notion clone 14v/notion/app/(secret)/documents/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DocumentPage = () => {
   return (
-    <div>sss</div>
+    <div></div>
   )
 }
 
