da18ce9778e23bcc35161480a4f7b0456322af09
NurMuhammad1988 <nurikyorov88@gmail.com>
2025-05-12 01:02:15 +0500

    cover.tsx is hard

--- 5-Modul. Notion clone 14v/notion/app/(secret)/documents/[documentId]/page.tsx
+++ 5-Modul. Notion clone 14v/notion/app/(secret)/documents/[documentId]/page.tsx
@@ -10,9 +10,10 @@ interface DocumentIdPageProps {
         documentId: Id<"documents">; //convexda convex/schema.tsda schema bilan nastroyka qilingan convex/document.tsda yozilgan createDocument functionida adress (ssilka) sifatida berib qo'yilgan "documents" ga convexda genereted qilib kelingan userni va documentlarni idsi global convex Id
 
         //yani convex documents papkada yani app/(secret)/documents/page.tsx failida uiga tayyorlangan document functionlarda failni dynamic yaratadigan functionlar bor shu dynamic yaratiladigan sahifalarni shu failida DocumentIdPage functionda bu keladigan dynamik sahifar returnda divga o'ralgan yani jsx ga o'ralgan shu sabab app/covex/documents.tsdagi server functionlar app/(secret)/documents/page.tsx failida ishlatilib uiga beriladi va bu fail esa shu bajarilgan ishlardagi dynamic sahifalarni jsxga o'girib beradi
-        //YANI FAIL BU DYNAMIC YARATILGAN YANI "ADD A PAGE" YOKIDA "NEW DOCUMENT" TEXTLARIGA BOSGANDA DYNAMIC YARATILADIGAN DOCUMENTLARNI RETURN ICHIDA JSX QIVOLISH YANI RETURNDA JSXDA RETURN QILISH
+        //YANI bu FAIL  DYNAMIC YARATILGAN YANI "ADD A PAGE" YOKIDA "NEW DOCUMENT" TEXTLARIGA BOSGANDA DYNAMIC YARATILADIGAN DOCUMENTLARNI RETURN ICHIDA JSX QIVOLISH YANI RETURNDA JSXDA RETURN QILISH yani jsx qilingan divga o'rab olish
     };
 }
+
 
 const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
     const document = useQuery(api.document.getDocumentById, {
--- 5-Modul. Notion clone 14v/notion/components/shared/cover.tsx
+++ 5-Modul. Notion clone 14v/notion/components/shared/cover.tsx
@@ -1,9 +1,11 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
+import { Button } from "../ui/button";
+import { ImageIcon } from "lucide-react";
 
 interface CoverProps {
-    url?: string;
+    url?: string; //url string qabul qiladi va  (secret)/documents/[documentId]/page.tsx failida ichida faqat string qabul qiladi (hozircha!!) yani coverni user hohlasa netdan url orqaliham olib kelib qo'yishi mumkun yani faqat devicedanmas ssilka bilanham olib kelib qo'yishi mumkun
     preview?: boolean;
 }
 
@@ -11,25 +13,39 @@ const Cover = ({ preview, url }: CoverProps) => {
     return (
         <div
             className={cn(
-                //cn bilan bu divda chaqiriladigan imageda agar url bor bo'lsa bu classlar>>>"relative w-full h-[35vh] group" agar uyo'q bo'lsa bu classlar>>>//cn bilan bu divda chaqiriladigan imageda agar url bor bo'lsa bu classlar>>>"relative w-full h-[35vh] group" va agar uyo'q bo'lsa bu classlar>>>!url && "h-[10vh]", url && "bg-muted" ishlasin deyildi
+                //cn bilan bu divda chaqiriladigan imageda agar url bor bo'lsa bu classlar>>>"relative w-full h-[35vh] group" agar yo'q bo'lsa bu classlar>>>
+                //cn bilan bu divda chaqiriladigan imageda agar url bor bo'lsa bu classlar>>>"relative w-full h-[35vh] group" va agar yo'q bo'lsa bu classlar>>>!url && "h-[10vh]", url && "bg-muted" ishlasin deyildi/////////yani uchta holat aytildi
                 "relative w-full h-[35vh] group",
-                !url && "h-[10vh]",
+                !url && "h-[10vh]", ////////yani agar url yo'q bo'lsa h-10 va urlni bg-muted yani ko'rinmeydigan qiladi yani url bilanchaqirilgan cover imageni joyini agar cover image yo'q bo'lsa ko'rinmas qilib qo'yadi
                 url && "bg-muted"
             )}
         >
             {!!url && (
                 <Image
                     fill
-                    src={url}
+                    src={url} //url string qabul qiladi va  (secret)/documents/[documentId]/page.tsx failida ichida faqat string qabul qiladi (hozircha!!) yani coverni user hohlasa netdan url orqaliham olib kelib qo'yishi mumkun yani faqat devicedanmas ssilka bilanham olib kelib qo'yishi mumkun
                     alt="cover rasmi "
                     className="object-cover "
                 />
             )}
-            {/* agar ona divda relative classi bor bo'lsa bu image aftamat tarzda absalute bo'lib hamma joyni egallab oladi?????? */}
-            {/* agarda url bor bo'lsa??? */}
+            {/* agar ona divda relative classi bor bo'lsa bu image aftamat tarzda absalute bo'lib hamma joyni egallab oladi yani ona divdagi hamma joyni masalan bu holatda cover imageni faqat shu>> w-full h-[35vh] joyga sig'diradi yani fill sabab! */}
+
+            {url &&
+                !preview && ( // urlda //agar url bor bo'lsa va preview false bo'lsa bu holat ishlasin bu holatda esa preview yo'q yani div ichida yokida button ichida bu bolean qiymatli preview chaqirilmadi shu sabab bu logical operator uchun preview endi false qaytaradi
+                    <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-20 flex items-center gap-x-2">
+                        {/* bu bola div ona divda esa relative classi va group classi bor yani endi bu divlar bir biriga bo'gliq holatga qarab ishlaydi masalan bu bola div boshida opacitysi 0 yani hech narsa ko'rinmeydi va group sabab  urlda kelgan imagega hover bo'lganda group-hover:opacity-100 ishleydi va pastdagi button uiga ko'rinadi yani mishkani cover imagega olib kelganda bu button ko'rinadi */}
+                        <Button
+                            size={"sm"}
+                            variant={"outline"}
+                            className="text-muted-foreground text-xs"
+                        >
+                            <ImageIcon />
+                            <span>Change cover</span>
+                        </Button>
+                    </div>
+                )}
         </div>
     );
 };
 
 export default Cover;
-
