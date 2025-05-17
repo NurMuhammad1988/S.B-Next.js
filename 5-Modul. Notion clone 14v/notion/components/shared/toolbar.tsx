f343a4d1357a3495cb559419ead11f81b3c54c9e
NurMuhammad1988 <nurikyorov88@gmail.com>
2025-05-18 02:16:49 +0500

    !!=?

--- 5-Modul. Notion clone 14v/notion/components/shared/toolbar.tsx
+++ 5-Modul. Notion clone 14v/notion/components/shared/toolbar.tsx
@@ -43,6 +43,14 @@ function Toolbar({ document, preview }: ToolbarProps) {
     return (
         <div className="pl-[54px] group relative">
             {!!document.icon && !preview && (
+                ///// let a = true;
+                ///// let b = !!a;
+                ///// console.log(b); =>>> true
+
+                //Yuqoridagi kod misolida birinchi let ibora ning qiymatini o'rnatadi true. Keyin, oldidagi ikkita undov belgisi a qiymatni inkor qiladi va b sifatida o'rnatiladi true.>>>>>>https://www.shecodes.io/athena/1080-what-do-two-exclamation-marks-mean-in-javascript#:~:text=In%20JavaScript%20and%20some%20programming,value%20of%20a%20boolean%20expression.
+
+                //yani bu holatda !!>>ikkita undov bilan document.iconni
+
                 // tailwindda group classlarga nom bersaham bo'ladi bu holatda group classimizni nomi icon>>> "group/icon"// agar documentda icon true bo'lsa va preview false bo'lsa bu holatda preview boshlang'ich holati oddiy boolean yani faqat true yoi false qabul qialdi shuni false qilib document.iconni true qilindi sabab bu logical operator false va true qiymatga qarab ishlaydi shu sabab  document.iconni tur qilish uchun prewievni false qivoldik va agar document.icon true bo'lsa pastdagi div ishlab unga click qilinganda IconPicker component ishlab ichidagi onIconChange function ishlaydi onIconChange functionda convex/document/updateFields functiondan kelgan documentni idisi  va iconi bor agar shu icon bor bo'lsa shu div ichidagi p tegi ishga tushadi qachonki hover bo'lsa va button ichidagi onRemoveIcon functionga click qilinsa serverdan kelgan icon remove bo'ladi yani>>> ""
                 //yani serverdan kelgan document.iconga hover bo'lganda X iconham ishga tushaid yani agar kerak bo'lsa remove qilish uchun shu uchun onRemoveIcon chaqirilgan buttonga group-hover/icon:opacity-100 classi berilgan yani onIconChange bor yani icon bor va shu iconga  hover qilingada X iconham bor yani hover qilinganda chiqadi va click qilinsa onRemoveIconham bor ishga tushadi va yana iconni udalit qilib document.iconniham previewniham false qiladi yani onRemoveIcon qilingandan keyin document.icon false bo'lib qoladi shunda bu>>>!document.icon && !preview &&  logical operator ishga tushib documentga icon qo'yish ishlaydi
                 <div className="flex items-center gap-x-2 group/icon pt-6">
@@ -71,7 +79,7 @@ function Toolbar({ document, preview }: ToolbarProps) {
 
             <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
                 {!document.icon &&
-                    !preview && ( //va yana agar document.icon ham false bo'lsa previewham false bo'lsa yani user document crete qilgandaham icon qo'ymagan bo'lsa yani yuqoridagi hech qaysi operatorlar ishlamasa yani hammasi false bo'lsa "emoji-picker-react"kutubhonasidan chaqirilgan tayyor ichida to'la kerakli emojilar bor IconPicker component ishga tushadi yani onIconChange function bilan birga ishga tushadi va user hohlagan iconini tanlab documentga qo'shib qo'yishi mumkun yani Add icon textiga click qilinganda convex/document.ts failidagi updateFields function ichidagi patch metodi sabab convexdagi objectga yani user yaratgan documentga icon qo'shiladi Smile iocon esa document.icon va prewiev qiymatlar false bo'lganda default ishlab turadi va bu div boshida opacity-0 bo'ladi yani ko'rinmeydi va hover bo'lganda group-hover:opacity-100 shu classlar sabab ko'rinadi va Add icon textiga click qilinganda agar document.iconfalse bo'lsa icon-picker.tsx failidagi functionlarishlab iconlar ishga tushadi va add qilinadi
+                    !preview && ( //va yana agar document.icon ham false bo'lsa previewham false bo'lsa yani user document crete qilgandaham icon qo'ymagan bo'lsa yani yuqoridagi hech qaysi operatorlar ishlamasa yani hammasi false bo'lsa "emoji-picker-react"kutubhonasidan chaqirilgan tayyor ichida to'la kerakli emojilar bor IconPicker component ishga tushadi yani onIconChange function bilan birga ishga tushadi va user hohlagan iconini tanlab documentga qo'shib qo'yishi mumkun yani Add icon textiga click qilinganda convex/document.ts failidagi updateFields function ichidagi patch metodi sabab convexdagi objectga yani user yaratgan documentga icon qo'shiladi Smile icon esa document.icon va prewiev qiymatlar false bo'lganda default ishlab turadi va bu div boshida opacity-0 bo'ladi yani ko'rinmeydi va hover bo'lganda group-hover:opacity-100 shu classlar sabab ko'rinadi va Add icon textiga click qilinganda agar document.icon false bo'lsa icon-picker.tsx failidagi functionlar ishlab iconlar ishga tushadi va add qilinadi
                         <IconPicker asChild onChange={onIconChange}>
                             <Button
                                 size={"sm"}
@@ -84,19 +92,21 @@ function Toolbar({ document, preview }: ToolbarProps) {
                         </IconPicker>
                     )}
 
-                {!document.coverImage && !preview && (
-                    <Button
-                        size={"sm"}
-                        variant={"outline"}
-                        className="text-muted-foreground text-xs"
-                    >
-                        <ImageIcon className="h-4 w-4 mr-2" />
-                        <span>Add cover</span>
-                    </Button>
-                )}
+                {!document.coverImage &&
+                    !preview && ( //document coverimage va preview  false bo'lsa
+                        <Button
+                            size={"sm"}
+                            variant={"outline"}
+                            className="text-muted-foreground text-xs"
+                        >
+                            <ImageIcon className="h-4 w-4 mr-2" />
+                            <span>Add cover</span>
+                        </Button>
+                    )}
             </div>
 
-            {!isEditing && !preview ? (shu koddan boshlab darsni qaytadan qil 9. Editor darsi 24chi minutdan boshla
+            {!isEditing && !preview ? (
+                // shu koddan boshlab darsni qaytadan qil 9. Editor darsi 24 chi minutdan boshlashu koddan boshlab darsni qaytadan qil 9. Editor darsi 24 chi minutdan boshlashu koddan boshlab darsni qaytadan qil 9. Editor darsi 24 chi minutdan boshlashu koddan boshlab darsni qaytadan qil 9. Editor darsi 24 chi minutdan boshla
                 <TextareaAutosize
                     ref={textareaRef}
                     onBlur={disableInput}
