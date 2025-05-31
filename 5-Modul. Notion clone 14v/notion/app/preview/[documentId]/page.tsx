bfe3754d7c7ef4d2eee69a966b324dba50a8c02f
NurMuhammad1988 <nurikyorov88@gmail.com>
2025-05-31 06:26:07 +0500

    preview is so hard

--- 5-Modul. Notion clone 14v/notion/app/preview/[documentId]/page.tsx
+++ 5-Modul. Notion clone 14v/notion/app/preview/[documentId]/page.tsx
@@ -1,7 +1,3 @@
-// import React, { useMemo } from "react";
-// import { Id } from "@/convex/_generated/dataModel";
-// import { useQuery } from "convex/react";
-
 "use client";
 
 import Cover from "@/components/shared/cover";
@@ -13,6 +9,7 @@ import { useQuery } from "convex/react";
 import dynamic from "next/dynamic";
 import React, { useMemo } from "react";
 
+//bu sahifada chaqirilgan toolbar va cover tsxlar prewiew qiymati true qilingan  yani preview qiymatlari berilganini sababi bu sahifada document dynamic yaratiladi yaratilgan sahifaga esa convex unique link beradi shu linkni yani documentni puplish qilib linkni boshqa userlarga jo'natganda linkdagi yangi sahifadagi postni boshqa userlar o'zgartiraolmaydi shu uchun preview qiymatlariga toolbar va cover tsx faillarida va operatori bilan mantiqiy vazifalar berilgan bu ikkala componentga  preview qiymati  berilmagan joylarda documentni create qilganuser hamma ishni qilaoladi lekin preview berilgan yani preview true qilinganbu sahifada yaratilgan documentda hech kim toolbar va cover tsxdan foydalanib documentni change qilaolmaydi
 interface DocumentIdPageProps {
     params: {
         documentId: Id<"documents">;
@@ -46,7 +43,6 @@ const Page = ({ params }: DocumentIdPageProps) => {
         );
     }
 
-
     if (document === null) return null;
 
     return (
@@ -60,8 +56,8 @@ const Page = ({ params }: DocumentIdPageProps) => {
 
                 <Editor
                     initialContent={document.content}
-                    onChange={() => {}}
-                    editable={false} //false bo'lgani uchun boshqa userlarga ishlamaydi faqat shu document egasi bo'lgan usergina ishlata oladi
+                    onChange={() => {}}//editordan props bilan kelgani function qabul qilishga majbur ekanligi sabab bo'sh function berib qo'yildi bo'masa hato qaytarishi mumkun
+                    editable={false} //false bo'lgani uchun boshqa userlarga ishlamaydi faqat shu document egasi bo'lgan usergina ishlata oladi yani editorni editable qiymati bu joyda yani document dycnamic yaratilib link berilgan joyda ishlamaydi
                 />
             </div>
         </div>
@@ -71,3 +67,4 @@ const Page = ({ params }: DocumentIdPageProps) => {
 export default Page;
 
 //bittagina hato borga o'hshaydi lekin hato yo'q yani yani agar user documentni crete qilib uni publish qilib publish qilingan documentni urlini copy qilib shu bu urlga alohida sahifada kirsa o'zi yaratgan documentni hech narsani change qilaolmayapti yani preview sabab va keyn user documenni unPublished qilsaham alohid sahifada ochilgan shu unpublished qilingan document baribir yo'qolib qolmayapti bu esa hatomas chunki user bitta browserdan foydalanayapti browser esa userni datalarini yani idilarini browserni Application sahifasida  Cookieslarda saqlab qolepti shu sabab user bitta browserdan kirganda unpublished qilinsaham documentni boshqa sahifadaham ko'ra olayapti lekin shu documentni unpublisheddan keyin boshqa borowserdan kirib ko'rilganda sahifa hato qaytarayapti chunki boshqa browserda userni cookilari yo'q va preview sabab boshqa userga bu document ko'rinmaydi
+
