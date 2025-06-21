97156c663bbbd07a0cb78dc17bef5c3b99cc3b19
NurMuhammad1988 <nurikyorov88@gmail.com>
2025-06-22 02:40:22 +0500

    stripe

--- 5-Modul. Notion clone 14v/notion/hooks/use-subscription.ts
+++ 5-Modul. Notion clone 14v/notion/hooks/use-subscription.ts
@@ -1,17 +1,17 @@
 import fetcher from "@/lib/fetcher";
-import useSwr from "swr";
+import useSwr from "swr"; //useSWR — bu React hook malumotni olib kelish (fetch yani ichida fetch bilan ishleydi), keshga saqlash, fonda yangilash//	URL orqali API yoki fayldan malumotni fetch qiladi BU HOLATDA APIDAN   //Fokusga qaytganda, vaqt otganda yoki mutate chaqirilganda avtomatik so'rov yani swrni  mutate functioni
 
 // bu hook email orqali Stripe obuna (subscription) holatini olish uchun ishlatiladi
 const useSubscription = (email: string) => {
     const { data, isLoading, mutate } = useSwr(
-        `/api/stripe/subscription?email=${email}`,//stripe API route manzili Bu joyda userning email manzili bilan birga Stripe subscription holatini olish uchun so'rov yuboriladi yani fetcher bilan yani bu api/stripe/subscription o'zimiz yozgan api bu useSubscription ishlaganda shu failga so'rov jo'natadi useSwr orqali brauzerda /api/stripe/subscription manziliga so'rov yuboriladi yani next js backendda
-        fetcher//Fetcher Stripega togridan-togri murojaat qilmaydi, u Next.js backend bilan gaplashadi. fetcher /api/stripe/subscription manziliga so'rov jonatadi
+        `/api/stripe/subscription?email=${email}`, //stripe API route manzili Bu joyda userning email manzili bilan birga Stripe subscription holatini olish uchun so'rov yuboriladi yani fetcher bilan yani bu api/stripe/subscription o'zimiz yozgan api bu useSubscription ishlaganda shu failga so'rov jo'natadi useSwr orqali brauzerda /api/stripe/subscription manziliga so'rov yuboriladi yani next js backendda
+        fetcher //Fetcher Stripega togridan-togri murojaat qilmaydi, u Next.js backend bilan gaplashadi. yani bu holatda /api/stripe/subscription ga so'rov jo'natadi u yerdagi api yani server backend functionlar esa  userni emailiga qarab data qaytaradi yani qaysi planda ekanligini qaytaradi va qaytgan dataga qarab sidebar qismidagi plan texti va document yaratish cheklovlari o'rnatiladi masalan returnda pastdagi plandagi datda "free" qaytsa faqat 3 da document agar "plus" yokida "biznes" planlar qaytsa plan nomlariham imtiyozlariham o'zgaradi shu uchun yozilgan kod
     );
 
     return {
-        plan:data,//stripe /api/stripe/subscription dan olingan subscription malumotni planga  sovoldi
+        plan: data, //stripe /api/stripe/subscription dan olingan subscription malumotni planga  sovoldi
         isLoading,
-        mutate,//chaqirilsa, ma’lumot qayta olib kelinadi (masalan, foydalanuvchi rejani o‘zgartirganda) yani swr ni functioni
+        mutate, //chaqirilsa, malumot qayta olib kelinadi (masalan, foydalanuvchi rejani o‘zgartirganda) yani swr ni functioni 
     };
 };
 
--- 5-Modul. Notion clone 14v/notion/lib/fetcher.ts
+++ 5-Modul. Notion clone 14v/notion/lib/fetcher.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const fetcher = (url: string) => axios.get(url).then((res) => res.data)
+//stripe subscriptionslarni aniqlashda kerak bo'ladiganb so'rovlarni shunday default qivoldik
+const fetcher = (url: string) => axios.get(url).then((res) => res.data)//get bilan urlga so'rov jo'natadi va res qilib datani oladi //get bilan urlga so'rov jo'natadi va res qilib datani oladi  yani shunda bitta kod so'rovni ham jo;natadi ham datalarni qayta qabul qiladi ///axios promise qaytaradi va unda res degan javob obyekti bo'ladi .then((res) => res.data) yhen yani keyin yani hali sihlashda davom etib then res objectga so'rovda kelgan datanisovoladi va bu use-subscription.ts failida chaqirib ishlatilgan
 
 export default fetcher
