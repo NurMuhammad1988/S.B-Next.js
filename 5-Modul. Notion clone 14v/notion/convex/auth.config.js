e92c5dc50bd5e08c773f0732b2bb83c7ec5c3625
NurMuhammad1988 <nurikyorov88@gmail.com>
2025-06-22 07:58:11 +0500

    Project ended

--- 5-Modul. Notion clone 14v/notion/convex/auth.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    providers: [
-        {
-            // bu function convexni eng ildiz provideri yani domain ichida convexga clerk ulandi yani endi convexda clerkda  user aftorizatsa qilish uchun ochilgan server ilovani adresi bor yani shu domaindagi https adrss bu clerkniki convex - clerk ulandi
-            domain: "https://wanted-duck-70.clerk.accounts.dev",
-            applicationID: "convex",
-        },
-    ],
-};
