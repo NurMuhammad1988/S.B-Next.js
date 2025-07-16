35ce349a7357ccfe89d71a21101e49272e8e9559
NurMuhammad1988 <nurikyorov88@gmail.com>
2025-07-17 03:18:45 +0500

    folders and firebase

--- 6-modul Google drive clone - 14v/google-drive/app/(root)/layout.tsx
+++ 6-modul Google drive clone - 14v/google-drive/app/(root)/layout.tsx
@@ -11,7 +11,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
             <Sidebar />
             {/* bu navbar va sidebar faqat accounti bor loyihada ro'yhatdan o'tgan user uchun ishlaydi shu uchun alohida (root) papka ichida alohida layout qilingan.... navbar app(auth)/layout.tsx chaqirilgan holatida esa ro'yhatdan o'tmagan user uchun ishlaydi yani app(auth)/layout.tsx holatida user navbardan foydalana olamaydi yani agar user register qilsa shu navbar isga tushadi yokida (auth) papka ichidagi navbar shunchaki uiga ko'rinib turadi (z-50 sabab!!!) */}
             <main className="w-full min-h-[90vh] relative top-[10vh] pl-72 bg-[#F6F9FC] dark:bg-[#1f1f1f] p-4 ">
-                <div className="h-[90vh] rounded-md bg-white dark:bg-black ml-4 p-8">
+                <div className="h-[85vh] rounded-md bg-white dark:bg-black ml-4 p-8">
                     {children}
                 </div>
             </main>
---
+++ 6-modul Google drive clone - 14v/google-drive/lib/firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "drive-86c64.firebaseapp.com",
+  projectId: "drive-86c64",
+  storageBucket: "drive-86c64.firebasestorage.app",
+  messagingSenderId: "215245272696",
+  appId: "1:215245272696:web:0b6349e14e57ad3f7b5f8d"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+!getApps().length ? initializeApp(firebaseConfig) : getApp()
+
+const db = getFirestore()
+
+export {db}
